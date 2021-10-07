e33a0adc8c4837ee7a16e48fe83cb8c06da2c13b
EBwilson <2534946881@qq.com>
2021-10-07 21:20:30 +0800

    a update

--- src/singularity/ui/dialogs/PublicInfoDialog.java
+++ src/singularity/ui/dialogs/PublicInfoDialog.java
@@ -1,15 +1,16 @@
 package singularity.ui.dialogs;
 
 import arc.Core;
-import arc.func.Prov;
+import arc.func.Cons;
 import arc.graphics.Pixmap;
 import arc.graphics.Texture;
 import arc.graphics.g2d.TextureRegion;
+import arc.scene.event.Touchable;
+import arc.scene.ui.Label;
 import arc.scene.ui.layout.Cell;
 import arc.scene.ui.layout.Table;
 import arc.struct.ObjectMap;
 import arc.struct.ObjectSet;
-import arc.struct.Seq;
 import arc.util.Http;
 import arc.util.Log;
 import arc.util.Strings;
@@ -17,59 +18,74 @@ import arc.util.Time;
 import arc.util.serialization.Jval;
 import singularity.Sgl;
 import universeCore.UncCore;
-import universeCore.util.animLayout.CellAction;
 import universeCore.util.animLayout.CellChangeColorAction;
+import universeCore.util.handler.FieldHandler;
 
+import java.util.concurrent.ExecutorService;
 import java.util.regex.Pattern;
 
 public class PublicInfoDialog extends BaseListDialog{
+  private static final String titlesUrl = "https://raw.githubusercontent.com/EB-wilson/Singularity/master/publicInfo/titles.hjson";
   private static final String langRegex = "#locale#";
   private static final Pattern imagePattern = Pattern.compile("<image *=.*>");
   
-  Seq<ItemEntry> itemEntrySeq;
-  boolean connected = false;
-  float timer = 0;
+  volatile boolean initialized, titleLoaded;
+  Jval titles;
+  
+  ObjectMap<String, Table> pages = new ObjectMap<>();
+  
+  Runnable loadPage;
+  LoadTable itemsLoading;
   
   public PublicInfoDialog(){
     super(Core.bundle.get("misc.publicInfo"));
     
-    Prov<String> loadAnim = () -> {
-      float time = Time.time % 120;
-      
-      return (time < 40? ".": time < 80? "..": "...") + "(" + Strings.autoFixed(timer*15, 0) + ")";
-    };
-    
-    defaultInfo = info -> {
-      info.add("").update(t -> {
-        t.setText(Core.bundle.get("misc.loading") + loadAnim.get());
-        
-        if(!connected){
-          timer += (1f/900)*Time.delta;
-    
-          if(timer >= 1){
-            infoTable.clearChildren();
-            infoTable.add(Core.bundle.get("warn.publicInfo.connectFailed"));
-            infoTable.row();
-            infoTable.button(Core.bundle.get("misc.refresh"), this::refresh).size(140, 60);
-      
-            Log.err("connect failed, time out");
-          }
-        }
-      });
+    defaultInfo = table -> {
+      if(!initialized){
+        itemsLoading = table.add(new LoadTable(this::refresh)).get();
+      }
+      else{
+        table.add(Core.bundle.get("dialog.PublicInfo.empty"));
+      }
     };
     
     shown(this::refresh);
   }
   
+  @SuppressWarnings("StatementWithEmptyBody")
   public void refresh(){
     Log.info("loading message");
+  
+    ExecutorService exec = FieldHandler.getValue(Http.class, "exec", null);
+    exec.shutdown();
+    Http.setMaxConcurrent(8);
     
-    connected = false;
-    timer = 0;
+    Http.get(titlesUrl, req -> {
+      titleLoaded = true;
+      titles = Jval.read(req.getResultAsString());
+    }, e -> {
+      titleLoaded = false;
+      infoTable.clearChildren();
+      infoTable.add(Core.bundle.get("warn.publicInfo.connectFailed"));
+      infoTable.row();
+      infoTable.button(Core.bundle.get("misc.refresh"), this::refresh).size(140, 60);
+  
+      Log.err(e);
+    });
+    
     String directory = Sgl.publicInfo + "directory.hjson";
-    itemEntrySeq = new Seq<>();
+    items.clear();
+    pages.clear();
+    
+    initialized = false;
   
     Http.get(directory, request -> {
+      while(!titleLoaded){
+      }
+      
+      itemsLoading.finish();
+      initialized = true;
+      
       String direResult = request.getResultAsString();
       Log.info(direResult);
       
@@ -78,13 +94,8 @@ public class PublicInfoDialog extends BaseListDialog{
         buildChild(jval);
       }
   
-      items = itemEntrySeq;
       rebuild();
-      
-      connected = true;
     }, e -> {
-      connected = false;
-      
       infoTable.clearChildren();
       infoTable.add(Core.bundle.get("warn.publicInfo.connectFailed"));
       infoTable.row();
@@ -100,11 +111,8 @@ public class PublicInfoDialog extends BaseListDialog{
   void buildChild(Jval sect){
     Log.info(name + ", " + sect.toString());
     
-    Table infoContainer = new Table();
-    infoContainer.defaults().grow().margin(margin).pad(pad);
     ObjectMap<String, TextureRegion> atlas = new ObjectMap<>();
     ObjectMap<String, float[]> atlasSize = new ObjectMap<>();
-    String[] title = {""};
     
     Jval.JsonMap assets = sect.get("assets").asObject();
     Log.info(assets.toString());
@@ -115,7 +123,8 @@ public class PublicInfoDialog extends BaseListDialog{
         atlasSize.put(asset.key, new float[]{size.get(0).asFloat(), size.get(1).asFloat()});
         
         if(asset.value.get("location").asString().equals("url")){
-          atlas.put(asset.key, Core.atlas.find("nomap"));
+          TextureRegion region;
+          atlas.put(asset.key, region = Core.atlas.find("nomap"));
           
           Runnable[] r = new Runnable[]{() -> {}};
           r[0] = () -> Http.get(asset.value.get("address").asString(), res -> {
@@ -124,7 +133,7 @@ public class PublicInfoDialog extends BaseListDialog{
               try{
                 Texture tex = new Texture(pix);
                 tex.setFilter(Texture.TextureFilter.linear);
-                atlas.put(asset.key, new TextureRegion(tex));
+                region.set(tex);
                 pix.dispose();
               }catch(Exception e){
                 Log.err(e);
@@ -150,57 +159,116 @@ public class PublicInfoDialog extends BaseListDialog{
     String language = !currLang.equals("en") && languages.contains(currLang)? currLang: "";
     String url = sect.get("info").asString().replace(langRegex, language);
     
-    Http.get(url).error(e -> {
+    loadPage = () -> {
       infoTable.clearChildren();
-      infoTable.add(Core.bundle.get("warn.publicInfo.connectFailed"));
-      infoTable.row();
-      infoTable.button(Core.bundle.get("misc.refresh"), this::refresh).size(140, 60);
-  
-      Log.err(e);
-    }).block(result -> {
-      String[] strs = result.getResultAsString().split("\n");
-      title[0] = strs[0];
-      Log.info(title[0]);
       
-      for(int i=1; i<strs.length; i++){
-        if(imagePattern.matcher(strs[i]).matches()){
-          String image = strs[i].replaceAll("<image *=", "").replace(">", "").replace(" ", "");
-          TextureRegion region = atlas.get(image);
-          float[] size = atlasSize.get(image);
-          
-          float iWidth = Math.min(width - margin*2 - pad - itemBoardWidth, size[0]);
-          float scl = iWidth/size[0];
-          
-          infoContainer.image(region).size(size[0]*scl, size[1]*scl);
-        }
-        else{
-          infoContainer.add(strs[i]).growX().fillY();
-        }
-        infoContainer.row();
+      if(pages.containsKey(url)){
+        infoTable.add(pages.get(url)).grow();
       }
-    });
+      else{
+        LoadTable loading = infoTable.add(new LoadTable(loadPage)).get();
+        
+        Http.get(url, result -> {
+          loading.finish();
+          
+          Table infoContainer = new Table();
+          infoContainer.defaults().grow().margin(margin).pad(pad);
+          String[] strs = result.getResultAsString().split("\n");
+  
+          for(String str : strs){
+            if(imagePattern.matcher(str).matches()){
+              String image = str.replaceAll("<image *=", "").replace(">", "").replace(" ", "");
+              TextureRegion region = atlas.get(image);
+              float[] size = atlasSize.get(image);
+      
+              float iWidth = Math.min(width - margin*2 - pad - itemBoardWidth, size[0]);
+              float scl = iWidth/size[0];
+      
+              infoContainer.image(region).size(size[0]*scl, size[1]*scl);
+            }else{
+              infoContainer.add(str).growX().fillY();
+            }
+            infoContainer.row();
+          }
     
-    Log.info(3);
+          pages.put(url, infoContainer);
+          loadPage.run();
+        }, e -> {
+          infoTable.clearChildren();
+          infoTable.add(Core.bundle.get("warn.publicInfo.connectFailed"));
+          infoTable.row();
+          infoTable.button(Core.bundle.get("misc.refresh"), loadPage).size(140, 60);
     
-    itemEntrySeq.add(new ItemEntry(table -> {
-      table.add(title[0]);
-      Log.info(title[0]);
+          Log.err(e);
+        });
+      }
+    };
+    
+    items.add(new ItemEntry(table -> {
+      table.add(title);
     }, table -> {
-      Cell<Table> cell = table.add(infoContainer).width(width - itemBoardWidth - pad - margin*2).growY().padTop(pad + 40).padBottom(pad).color(infoContainer.color.cpy().a(0));
-      UncCore.cellActions.add(new CellChangeColorAction(cell, table, infoContainer.color.cpy().a(1), 30));
-      UncCore.cellActions.add(new CellAction(cell, table, 30){
-        float p = pad + 40;
-        float to = pad;
-        float curr = pad + 40, currP = pad;
-
-        @Override
-        public void action(){
-          curr = p + (to - p)*progress;
-          currP = p + (to - p)*(1-progress);
-          cell.padTop(curr);
-          cell.padBottom(currP);
-        }
-      }.gradient(0.2f));
+      loadPage.run();
     }));
+  }
+  
+  public static class LoadTable extends Table{
+    float waitTime;
+    
+    float time;
+    boolean timeout, lastTest = false, connectSucceed;
+  
+    public Cons<Label> buildLoading = t -> t.setText(Core.bundle.get("misc.loading") + Strings.autoFixed(time/60, 0) + getLoading(3, 30));
+    Runnable refresh;
+  
+    Cell<Table> cell;
+    
+    public static String getLoading(int length, float interval){
+      float time = Time.time % interval;
+      int amount = (int)Math.ceil((time/interval)*length);
+      
+      String result = ".".repeat(amount);
+      return result + " ".repeat(length - amount);
+    }
+    
+    public LoadTable(Runnable refresh){
+      this(9000, refresh);
+    }
+    
+    public LoadTable(float wait, Runnable refresh){
+      this.refresh = refresh;
+      waitTime = wait;
+  
+      add("").update(buildLoading);
+      row();
+      cell = table(t -> {
+        t.add(Core.bundle.get("dialog.publicInfo.loadTimeout"));
+        t.button(Core.bundle.get("misc.refresh"), () -> {
+          lastTest = timeout = connectSucceed = false;
+          time = 0;
+          UncCore.cellActions.add(new CellChangeColorAction(cell, this, t.color.cpy().a(0), 60));
+  
+          ExecutorService exec = FieldHandler.getValue(Http.class, "exec", null);
+          exec.shutdown();
+          Http.setMaxConcurrent(8);
+          
+          refresh.run();
+        }).get().touchable(() -> timeout? Touchable.enabled: Touchable.disabled);
+      });
+      cell.color(cell.get().color.cpy().a(0));
+      
+      cell.update(t -> {
+        if(!connectSucceed) time += Time.delta;
+        if(time > waitTime) timeout = true;
+        if(!lastTest && timeout){
+          lastTest = true;
+  
+          UncCore.cellActions.add(new CellChangeColorAction(cell, this, t.color.cpy().a(1), 60));
+        }
+      });
+    }
+    
+    public void finish(){
+      connectSucceed = true;
+    }
   }
 }
